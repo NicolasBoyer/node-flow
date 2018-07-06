4e6345c4d7f6273ed5c55f582ad2b2a279e4aa71
Unknown <elendil_le_grand@hotmail.com>
2018-07-06 09:09:41 +0200

    feat(wapitis): service worker + update

---
+++ src/www/sw-save.js
@@ -0,0 +1,71 @@
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+    "index.html",
+    "./", // Alias for index.html
+    "bundle.js",
+    "polyfills.js",
+    "assets/f88b6d21-icons.svg",
+    "manifest.json"
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener("install", event => {
+    event.waitUntil(
+        // caches.delete(PRECACHE)
+        caches.open(PRECACHE)
+        .then(cache => cache.addAll(PRECACHE_URLS))
+        .then(self.skipWaiting())
+    );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener("fetch", event => {
+    // Skip cross-origin requests, like those for Google Analytics.
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+        
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
+
+// Tester la présentation du bouton ...
+// https://developers.google.com/web/updates/2018/06/a2hs-updates : utiliser ça dans speedui
+// se servir de  https://github.com/wildhaber/offline-first-sw/blob/master/sw.js pour créer le service worker
+// window.addEventListener("beforeinstallprompt", (e) => {
+//     e.preventDefault();
+//     deferredPrompt = e;
+//     console.log
+//     // btnAdd.style.display = "block";
+// });
